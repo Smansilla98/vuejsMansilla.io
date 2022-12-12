3a17110fa5569782725e6b8abf73991911f22c9a
Smansilla98 <santeemansilla@gmail.com>
2022-12-12 19:00:04 -0300

    Fix vueJs

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
 var app = new Vue({
     el: '#app',
     data:{
-        nombre1:"Carlos",
-        profesion1:"Carpintero",
-        fecha1:"11/02/1999",
-        nombre2:"Ramon",
-        profesion2:"Verdulero",
-        fecha2:"21/12/1959",
-        nombre3:"Oscar",
-        profesion1:"Odontologo",
-        fecha3:"11/05/1982"
+        profesionales: [
+        {
+            id: 1,
+            nombre:"Carlos",
+            profesion:"Carpintero",
+            fecha:"11/02/1999"
+        },
+        {
+            id: 2,
+            nombre:"Ramon",
+            profesion:"Verdulero",
+            fecha:"21/12/1959"
+        },
+        {
+            id: 3,
+            nombre:"Oscar",
+            profesion:"Odontologo",
+            fecha:"11/05/1982"
+        }
+        ]
 
     }
     
--- prueba-componente.js
+++ prueba-componente.js
@@ -4,24 +4,27 @@ Vue.component('prueba-componente',{
             nombre:"Pepe"
         }
     },
-    props:['nombre','profesion', 'fecha'],
-    template:/*html*/`
-    
-    <table class="table table-dark">
-  <thead>
-    <tr>
-      <th scope="col">Nombre</th>
-      <th scope="col">Profesion</th>
-      <th scope="col">Fecha</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>{{nombre}}</td>
-      <td>{{profesion}}</td>
-      <td>{{fecha}}</td>
-    </tr>
-  </tbody>
-</table>
+    props:{
+        nombre:String,
+        profesion:String,
+        fecha:String,
+    },
+    template:/*html*/`    
+    <table class="table">
+    <thead>
+        <tr>
+        <th scope="col">Nombre</th>
+        <th scope="col">Profesion</th>
+        <th scope="col">Fecha</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <td>{{nombre}}</td>   
+        <td>{{profesion}}</td>
+        <td>{{fecha}}</td>
+        </tr>        
+    </tbody>
+    </table>
     `
 })
